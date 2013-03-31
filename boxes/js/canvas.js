1853cbe08994a21764a0d7264bf9f26c8f99e2dd
Tami Forrester <tforrest@mit.edu>
2013-03-31 19:10:08 -0400

    added canvas function outline

---
+++ boxes/js/canvas.js
@@ -0,0 +1,51 @@
+/**
+ * Canvas represents the state of the screen's canvas, where users can drag and connect boxes.
+ **/
+var canvas = function(){
+    this.blocks = {};
+    
+    /**
+     *Adds a new block to the canvas
+     */
+    this.add = function(block_id){
+        
+    }
+    /*
+     *Removes a block from the canvas
+     */
+    this.remove = function(block_id){
+        
+    }
+    /**
+     * Returns the block
+     */
+    this.getBlock = function(block_id){
+        
+    }
+    
+    /**
+     *Saves the current canvas
+     */
+    this.save = function(){
+        
+    }
+    /**
+     *Clears the canvas and removes all blocks
+     */
+    this.clear = function() {
+        
+    }
+    /**
+     *Dispatches a new block event to all event listeners of the given type
+     */
+    this.dispatchBlockEvent = function(type, details){
+        
+    }
+    
+    /**
+     *returns the canvas to script
+     */
+    this.toScript = function(){
+        
+    }
+}
